91fa9b3afb1d5e546ac048d844ce5c9dca601ff6
Vanya Usalko <dk4aol@gmail.com>
2024-06-04 15:30:03 +0300

    # I've updated TODO.

--- service/tests/test_list_dags.py
+++ service/tests/test_list_dags.py
@@ -3,7 +3,10 @@ from os import execvp
 
 
 class TestListDags(TestCase):
-    
+
     def test1(self):
-        #TODO: initialize database before tests.
+        # TODO: initialize database before tests.
+        # p = os.popen("ps -eo pid,ppid,args")
+        # output = p.read()
+        # p.close()
         execvp('.venv/bin/airflow', ['dags', 'list'])
