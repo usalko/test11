f80d1569af61ee599ec1e5ff121718179c89636d
Vanya Usalko <dk4aol@gmail.com>
2024-06-04 19:38:37 +0300

    # There is the first implementation of test list dags. # There is an airflow configuration.

--- service/tests/test_list_dags.py
+++ service/tests/test_list_dags.py
@@ -1,5 +1,17 @@
+from os import _wrap_close
+from subprocess import PIPE, Popen
+from typing import Any, Dict
 from unittest import TestCase
-from os import execvp
+
+
+def popen(command: str, env: Dict[str, Any] = {}):
+    proc = Popen(command,
+                 shell=True,
+                 text=True,
+                 stdin=PIPE,
+                 bufsize=4096,
+                 )
+    return _wrap_close(proc.stdin, proc)  # type: ignore
 
 
 class TestListDags(TestCase):
@@ -9,4 +21,10 @@ class TestListDags(TestCase):
         # p = os.popen("ps -eo pid,ppid,args")
         # output = p.read()
         # p.close()
-        execvp('.venv/bin/airflow', ['dags', 'list'])
+
+        with popen(f'.venv/bin/airflow db init', env={}) as process:
+            print(process.read())
+
+        with popen(f'.venv/bin/airflow dags list') as process:
+            dags_list_result = process.read()
+        self.assertIsNotNone(dags_list_result)
